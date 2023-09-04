7b6a4bc7ba34986ca8a232ba6bc4358761ee2977
poojamansukhani <poojamansukhani94@gmail.com>
2023-09-04 17:39:22 +0530

    react nested child

--- App.js
+++ App.js
@@ -1,4 +1,21 @@
+const parent = React.createElement("div", {id:"parent"},
+        [
+            React.createElement("div", {id:"child"}, 
+                [
+                    React.createElement("h1", {},"I am heading from parent"),
+                    React.createElement("h2", {},"I am heading 2 from parent")
+                ],
+            ),
+            React.createElement("div", {id:"child2"}, 
+                [
+                    React.createElement("h1", {},"I am heading 2 from parent"),
+                    React.createElement("h2", {},"I am heading 3 from parent")
+                ],
+            )
+        ]
+)
+
 const heading = React.createElement("h1", {id:"heading", xyz:"abc"}, "Hello from React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-console.log(heading)
+root.render(parent);
+console.log(parent)
