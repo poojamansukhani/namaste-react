88e090c91e8a139fecb863a68ae3af9e6d734230
poojamansukhani <poojamansukhani94@gmail.com>
2023-09-11 19:12:03 +0530

    functional component fundamentals

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 //React Element Normal JS variable which is containing object at the end
+const ele = <span>React</span>
 const jsxHeading = (
-    <h1 className="heading">Hi</h1>
+    <h1 className="heading">{ele} Hi from React Element</h1>
     );
-    const TitleComponent = () => {
-        return <h1>Hi coming fromm Title Functional Component</h1>
-    }
+    //Components
+const TitleComponent = () => {
+    return <h1>Hi coming fromm Title Functional Component</h1>
+}
 const HeadingComponent = () => (
     <div className="container">
+        {/*Using react element in react component*/}
+        {jsxHeading}
         <TitleComponent/>
+        {/*You can call functional componenet becasue at the end it is JS function */}
+        {TitleComponent()}
+        <TitleComponent></TitleComponent>
         <h1>Hi coming fromm Functional Component</h1>
+        {1+1}
     </div>
 )
 
