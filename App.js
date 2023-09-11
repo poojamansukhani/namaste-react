eedfe07adfbe49ba9aec8b592b437b7f563d76df
poojamansukhani <poojamansukhani94@gmail.com>
2023-09-11 18:09:59 +0530

    jsx

--- App.js
+++ App.js
@@ -1,23 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", {id:"parent"},
-        [
-            React.createElement("div", {id:"child"}, 
-                [
-                    React.createElement("h1", {},"I am heading from parent"),
-                    React.createElement("h2", {},"I am heading 2 from parent")
-                ],
-            ),
-            React.createElement("div", {id:"child2"}, 
-                [
-                    React.createElement("h1", {},"I am heading 2 from parent"),
-                    React.createElement("h2", {},"I am heading 3 from parent")
-                ],
-            )
-        ]
-)
-
-const heading = React.createElement("h1", {id:"heading", xyz:"abc"}, "Hello from React");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-console.log(parent)
+//React Element Normal JS variable which is containing object at the end
+const jsxHeading = (
+    <h1 className="heading">Hi</h1>
+    );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(jsxHeading)
