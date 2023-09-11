e2d96991d79d270cf8e12f39299bb06b310e6108
poojamansukhani <poojamansukhani94@gmail.com>
2023-09-11 18:39:47 +0530

    functional component

--- App.js
+++ App.js
@@ -4,5 +4,18 @@ import ReactDOM from "react-dom/client";
 const jsxHeading = (
     <h1 className="heading">Hi</h1>
     );
+    const TitleComponent = () => {
+        return <h1>Hi coming fromm Title Functional Component</h1>
+    }
+const HeadingComponent = () => (
+    <div className="container">
+        <TitleComponent/>
+        <h1>Hi coming fromm Functional Component</h1>
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(jsxHeading)
+//React element rendering
+// root.render(jsxHeading)
+//React component rendering
+root.render(<HeadingComponent/>)
