34425c244a09846c38a521ccd9f217935483964c
poojamansukhani <poojamansukhani94@gmail.com>
2023-09-13 11:51:47 +0530

    state

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,28 @@
 import RestroCard from "./RestroCard";
 import resLists from "../utils/mockData.js";
+import { useState } from "react";
 const Body = () => {
+    const [lists, setLists] = useState(resLists)
     return(
         <div className="body">
             <div className="container">
-                <div className="search">
-                    Search
+                <div className="filter">
+                    <button type="button" className="btn filter-btn" onClick={()=>{
+                        let data = lists.filter(
+                            (res) => res.data.rating > 4
+                        );
+                        setLists(data);
+                    }}>
+                        Top Rated Restaurents
+                    </button>
+                    <button type="button" className="btn filter-btn" onClick={()=>{
+                       
+                        setLists(resLists);
+                    }}>Show All</button>
                 </div>
                 <div className="restro-container grid">
                     {
-                        resLists.map((restaurent)=>{
+                        lists.map((restaurent)=>{
                             return <RestroCard key={restaurent.data.id} resData = {restaurent}/>
                         })
                     }
--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -1,4 +1,4 @@
-const resLists = [
+let resLists = [
     {
         type:"restaurent",
         data:{
@@ -18,11 +18,11 @@ const resLists = [
             id:"335",
             name:"kfc",
             address:"69, Whitefield",
-            rating:"4.5",
+            rating:"3",
             cusine:["Fries", "Burger"],
             priceForTwo:"300",
             time:"30min",
-            image:"e33e1d3ba7d6b2bb0d45e1001b731fcf"
+            image:"56c9ab92bd79745fd152a30fa2525426"
         }
     },
     {
@@ -31,7 +31,7 @@ const resLists = [
             id:"336",
             name:"Meghna Food",
             address:"69, Brrokfield",
-            rating:"4.8",
+            rating:"3.9",
             cusine:["Biryani", "Kari"],
             priceForTwo:"300",
             time:"30min",
@@ -49,6 +49,110 @@ const resLists = [
             priceForTwo:"250",
             time:"30min",
             image:"fjqcvqfgqlw6h0atques"
+        }
+    },
+    {
+        type:"restaurent",
+        data:{
+            id:"338",
+            name:"Sharief Bhai",
+            address:"69, Brrokfield",
+            rating:"4.2",
+            cusine:["Biryani", "Mughlai", "CokArabiane", "Beverages"],
+            priceForTwo:"400",
+            time:"30min",
+            image:"eahrfj70zhzy621zi8yq"
+        }
+    },
+    {
+        type:"restaurent",
+        data:{
+            id:"339",
+            name:"Lavonne",
+            address:"69, Brrokfield",
+            rating:"4.6",
+            cusine:["Bakery", "Desserts"],
+            priceForTwo:"750",
+            time:"30min",
+            image:"emlehbuwgsmryxhwzhvq"
+        }
+    },
+    {
+        type:"restaurent",
+        data:{
+            id:"340",
+            name:"Burger King",
+            address:"69, Brrokfield",
+            rating:"4.1",
+            cusine:["Burgers", "American"],
+            priceForTwo:"350",
+            time:"30min",
+            image:"e33e1d3ba7d6b2bb0d45e1001b731fcf"
+        }
+    },
+    {
+        type:"restaurent",
+        data:{
+            id:"341",
+            name:"McDonald's",
+            address:"69, Brrokfield",
+            rating:"4.2",
+            cusine:["Burgers", "Beverages","Cafe","Desserts"],
+            priceForTwo:"350",
+            time:"35min",
+            image:"ee5f8e06b300efc07c9fe3f4df40dfc4"
+        }
+    },
+    {
+        type:"restaurent",
+        data:{
+            id:"342",
+            name:"Pizza Hut",
+            address:"69, Brrokfield",
+            rating:"4",
+            cusine:["Pizzas"],
+            priceForTwo:"350",
+            time:"35min",
+            image:"2b4f62d606d1b2bfba9ba9e5386fabb7"
+        }
+    },
+    {
+        type:"restaurent",
+        data:{
+            id:"343",
+            name:"Domino's Pizza",
+            address:"69, Brrokfield",
+            rating:"4.3",
+            cusine:["Pizzas","Italian","Pastas","Desserts"],
+            priceForTwo:"350",
+            time:"35min",
+            image:"fjqcvqfgqlw6h0atques"
+        }
+    },
+    {
+        type:"restaurent",
+        data:{
+            id:"344",
+            name:"The Belgian Waffle Co.",
+            address:"69, Brrokfield",
+            rating:"4.3",
+            cusine:["Waffle","Ice Cream","Beverages","Desserts"],
+            priceForTwo:"200",
+            time:"35min",
+            image:"8cb220fa3997a3b928d2ffb6c098acaf"
+        }
+    },
+    {
+        type:"restaurent",
+        data:{
+            id:"345",
+            name:"Natural Ice Cream",
+            address:"69, Brrokfield",
+            rating:"4.7",
+            cusine:["Pizzas","Italian","Pastas","Desserts"],
+            priceForTwo:"150",
+            time:"35min",
+            image:"on12tk5aqza0stxxsqhc"
         }
     }
 ]
