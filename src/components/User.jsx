cbf40367b362cfd94b150638563658b5c145c45f
poojamansukhani <poojamansukhani94@gmail.com>
2023-09-17 18:49:20 +0530

    class func state, props update

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 const User = (props) =>{
+    const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(1);
     return(
         <div className="container">
             <div className="user-card">
+                    <h1>Count = {count}</h1>
+                    <h1>Count2 = {count2}</h1>
+                    <button onClick={()=>setCount(count+1)}>Increase</button>
                     <h2>Name : {props.name}</h2>
                     <h3>Location : {props.location}</h3>
-
             </div>
         </div>
     )
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 class UserClass extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            count:0,
+            count2:1,
+        }
     }
     render(){
         const {name, location} = this.props;
         return(
-            <div className="container">
+            <div className="container">:
                 <div className="user-card">
+                <h1>Count2 : {this.state.count2}</h1>
+                    <h1>Count : {this.state.count}</h1>
+                    <button onClick={()=>this.setState({
+                        count:this.state.count+1
+                        })
+                        }>Increase</button>
                     <h2>Name : {name}</h2>
                     <h3>Location : {location}</h3>
 
